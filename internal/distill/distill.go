a82ba58ef08b60b65b46e90b9884d875fdb3e0c3
Andrea Giacobino <no.andrea@gmail.com>
2018-05-13 21:48:13 +0200

    Refactor project files

--- internal/distill/distill.go
+++ internal/distill/distill.go
@@ -11,8 +11,18 @@ import (
 	"time"
 
 	"github.com/jbrodriguez/mlog"
+	gonanoid "github.com/matoous/go-nanoid"
 	"gitlab.com/welance/distill/internal"
 )
+
+// generateID generates a new id
+func generateID() (shortID string) {
+	a := internal.Config.ShortID.Alphabet
+	l := internal.Config.ShortID.Length
+	// a and l are validated before
+	shortID, _ = gonanoid.Generate(a, l)
+	return
+}
 
 // UpsertURLSimple insert or updae an url
 // shortcut for UpsertURL(true, true, time.Now())
